61fd382235e3d7aa982e98e28a319ef976d223ae
Aam1738 <adamwesleygreen@gmail.com>
2024-07-09 10:03:11 -0400

    Asset updates

--- Assets/Scripts/enemy_basic.gd
+++ Assets/Scripts/enemy_basic.gd
@@ -10,18 +10,17 @@ extends enemy
 
 func set_target(target: plant):
 	target_body = target
-
-func _physics_process(delta):
-	anim.play("run")
-	if target_body == null:
-		print("Enemy has no player reference!")
-		pass
-	else:
-		if not is_target_to_the_left():
-			anim.flip_h = true
-		direction = (target_body.global_position - global_position).normalized()
-		velocity = velocity.lerp(direction * speed, accel * delta)
-		move_and_slide()
-
-func is_target_to_the_left():
-	return target_body.position.x < position.x
+#
+#func _physics_process(delta):
+	#anim.play("run")
+	#if target_body == null:
+		#print("Enemy has no player reference!")
+	#else:
+		#if not is_target_to_the_left():
+			#anim.flip_h = true
+		#direction = (target_body.global_position - global_position).normalized()
+		#velocity = velocity.lerp(direction * speed, accel * delta)
+		#move_and_slide()
+#
+#func is_target_to_the_left():
+	#return target_body.position.x < position.x
